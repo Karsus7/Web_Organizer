40569f6efca37704817d77fce55e03e2f16fe4c7
Ty D'Angelo <tydangelo18@gmail.com>
2020-08-01 11:10:28 -0500

    Created User Model Using Sequelize.

--- models/user.js
+++ models/user.js
@@ -1,3 +1,38 @@
-// id
-// username
-// password
+/**
+ * TODO: require bcrypt to authenticate password
+ * ? Should we do this now or wait? 
+ */
+
+ // User Model
+module.exports = function(sequelize, DataTypes) {
+    let User = sequelize.define("User", {
+        // User must have an email to use app
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        // User Password
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+   /**
+    * TODO: Validate password by comparing inputted password to password stored in database
+    * 
+    */
+
+    
+    // One User-to-Many Bookmarks Relationship
+    User.associate = function(models) {
+        User.hasMany(models.Bookmark, {
+            onDelete: "cascade"
+        });
+    };
+
+ return User;
+};
