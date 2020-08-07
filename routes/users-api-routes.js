0c8f7a158d8280a151d35c92055edffd88c6f955
Lia <liavibanez@gmail.com>
2020-08-06 22:34:58 -0500

    removing add category button

--- routes/users-api-routes.js
+++ routes/users-api-routes.js
@@ -31,7 +31,6 @@ module.exports = function(app) {
   // User Route: Create new User
   app.post("/api/users", function(req, res) {
     console.log(req.body);
-    console.log ('called controller??')
     // Object to enter (create) into user table
     db.User.create({
       email: req.body.email,
