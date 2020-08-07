b4908887777d1bbf200c8468b00b6a5de792ff75
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-08-07 18:00:27 -0500

    logout now working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 // Requiring our models and passport as we've configured it
-var db = require("../models");
-var passport = require("../config/passport");
+const db = require("../models");
+const passport = require("../config/passport");
+const session = require('express-session');
 
 module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
@@ -28,7 +29,8 @@ module.exports = function(app) {
 
   // Route for logging user out
   app.get("/logout", function(req, res) {
-    req.logout();
+    // req.session.destroy()
+    req.logout()
     res.redirect("/");
   });
 
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,27 +1,36 @@
 // Require path
 let path = require("path");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 // Front-End Routes
 module.exports = function(app) {
 
     // Index Route: Login page for users to login
     app.get('/', function(req, res){
-        res.render('login');
-    })
-
-    // Signup Route: Sign Up Page for Users to create an account
-    app.get('/signup', function(req, res){
+        if (req.user){
+        res.redirect('/members');
+    };
         res.render('signup');
     })
 
+    // Signup Route: Sign Up Page for Users to create an account
+    app.get('/login', function(req, res){
+       if (req.user){
+        res.redirect('/members');
+    };
+        res.render('login');
+
+    })
+
     // Users Route: Page a signed in user will view
-    app.get('/members', function(req, res){
+    app.get('/members',isAuthenticated, function(req, res){
         res.render('members');
+
     })
     
     // making this explicit because of link on signup page to login page
-    app.get('/login', function(req, res){
-        res.render('login');
-    })
+    // app.get('/setup', function(req, res){
+    //     res.render('setup');
+    // })
 
 }
