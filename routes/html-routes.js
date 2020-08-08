27b3532494da7810ab97646bee981c861ccb816c
Lia <liavibanez@gmail.com>
2020-08-08 13:44:16 -0500

    adding delete route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -25,22 +25,11 @@ module.exports = function(app) {
 
     // Users Route: Page a signed in user will view
     app.get('/members', function(req, res){
-
-        //POST ROUTE
-        // api_helper.api_post('http://localhost:8080/api/bookmarks', {
-        //     UserId: 1,
-        //     url: 'youtube.com',
-        //     category: 'tutorials',
-        //     keyword: 'express',
-        //     createdAt: '2020-08-07 22:30:22',
-        //     updatedAt: '2020-08-07 22:30:22'
-        // })
-
         //DELETE ROUTE
         //need to pass through ID
         // api_helper.api_delete('http://localhost:8080/api/bookmarks/1').then()
 
-        api_helper.api_get('http://localhost:8080/api/bookmarks').then(response => {
+        api_helper.api_get('http://localhost:8080/api/bookmark').then(response => {
             res.render('members', {
                 bookmarks: response
             })
@@ -49,8 +38,16 @@ module.exports = function(app) {
         })
     });
 
-    //post - > send server data received from webpage.
-    // app.post()
+    //delete
+    // app.get('/members', function(req, res){
+    //     api_helper.api_delete('http://localhost:8080/api/bookmarks/:id').then(
+    //         res.render('members', {
+    //             bookmarks: response
+    //         })
+    //     ).catch(error => {
+    //         res.send(error)
+    //     })
+    // });
 
     app.get('/members',isAuthenticated, function(req, res){
         res.render('members');
