4eacbf3b021d98118030f869d07f98833f00f666
Lia <liavibanez@gmail.com>
2020-08-10 08:05:12 -0500

    jaws issues

--- config/config.js
+++ config/config.js
@@ -17,18 +17,15 @@ module.exports = {
     },
     production: {
       username: 'root',
-      use_env_variable : JAWSDB_URL,
+      password: 'null',
+      database: 'database_production',
       host: '127.0.0.1',
       dialect: 'mysql'
   }
-  
-  // if (process.env.JAWSDB_URL) {
-  //   connection = mysql.createConnection(process.env.JAWSDB_URL);
-  // }else {
-  //   connection = mysql.createConnection({
-  //     host: 'localhost',
-  //     user: 'root',
-  //     password: process.env.DB_PASSWORD,
-  //     database: process.env.DB_NAME
-  //   })
 }
+
+// "username": "root",
+        // "password": null,
+        // "database": "database_production",
+        // "host": "127.0.0.1",
+        // "dialect": "mysql"
