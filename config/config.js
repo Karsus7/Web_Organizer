91f227e8a2e1bf7c5d9246dfa586db8df32655d2
Lia <liavibanez@gmail.com>
2020-08-22 17:37:26 -0500

    project 2

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 module.exports = {
   "development": {
     "username": "root",
-    "password": "process.env.DB_PASSWORD",
-    "database": "process.env.DB_NAME",
+    "password": "password",
+    "database": "weborgdb",
     "host":     "127.0.0.1",
     "dialect":  "mysql"
   },
@@ -15,24 +15,24 @@ module.exports = {
 }
 
 
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
-var connection;
-if (process.env.JAWSDB_URL) {
-    // Database is JawsDB on Heroku
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    // Database is local
-    connection = mysql.createConnection({
-        port: 3306,
-        host: 'localhost',
-        user: 'root',
-        password: 'process.env.DB_PASSWORD',
-        database: 'process.env.DB_NAME'
-    })
-};
+// var connection;
+// if (process.env.JAWSDB_URL) {
+//     // Database is JawsDB on Heroku
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     // Database is local
+//     connection = mysql.createConnection({
+//         port: 3306,
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'process.env.DB_PASSWORD',
+//         database: 'process.env.DB_NAME'
+//     })
+// };
 
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
 
 
