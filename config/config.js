41f238691a320de2af9cfa4f887a3752e409713c
Lia <liavibanez@gmail.com>
2020-08-24 21:42:39 -0500

    commenting out dev portion

--- config/config.js
+++ config/config.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 console.log(process.env.NODE_ENV);
 
 module.exports = {
-	development: {
-		username: 'root',
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
-		host: '127.0.0.1',
-		dialect: 'mysql',
-	},
+	// development: {
+	// 	username: 'root',
+	// 	password: process.env.DB_PASSWORD,
+	// 	database: process.env.DB_NAME,
+	// 	host: '127.0.0.1',
+	// 	dialect: 'mysql',
+	// },
 	production: {
 		use_env_variable: 'JAWSDB_URL',
 		dialect: 'mysql',
