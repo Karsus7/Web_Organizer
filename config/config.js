ea49f47ed06e97940b432e053585d050dcc29010
Lia <liavibanez@gmail.com>
2020-08-24 22:11:10 -0500

    change env variables

--- config/config.js
+++ config/config.js
@@ -3,32 +3,16 @@ require('dotenv').config();
 console.log(process.env.NODE_ENV);
 
 module.exports = {
-	// development: {
-	// 	username: 'root',
-	// 	password: process.env.DB_PASSWORD,
-	// 	database: process.env.DB_NAME,
-	// 	host: '127.0.0.1',
-	// 	dialect: 'mysql',
-	// },
+	development: {
+		username: 'root',
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+		host: '127.0.0.1',
+		dialect: 'mysql',
+	},
 	production: {
 		use_env_variable: 'JAWSDB_URL',
 		dialect: 'mysql',
 	},
 };
 
-var mysql = require('mysql');
-
-var connection;
-if (process.env.JAWSDB_URL) {
-    // Database is JawsDB on Heroku
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    // Database is local
-    connection = mysql.createConnection({
-        port: 3306,
-        host: 'localhost',
-        user: 'root',
-        password: 'process.env.DB_PASSWORD',
-        database: 'process.env.DB_NAME'
-    })
-};
